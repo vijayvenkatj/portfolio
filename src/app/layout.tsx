d0e18f1365c2a925a88e4a06c2c045974c7dc9bf
vijayvenkatj <vijayvenkatj@gmail.com>
2025-03-15 22:45:12 +0530

    added subset to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StickyGlassNavbar from "@/components/navbar";
 
-const inter = Inter();
+const inter = Inter({ subsets: ["latin-ext"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
