936d027ae54ef89e3a792e6e0f0c09d3cb823fd0
vijayvenkatj <vijayvenkatj@gmail.com>
2025-05-01 17:52:37 +0530

    Added Granville Experience and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StickyGlassNavbar from "@/components/navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin-ext"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       >
         <StickyGlassNavbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
