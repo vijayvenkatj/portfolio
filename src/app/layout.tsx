2c877f97c6a50c9d04e514ff318cc5d2a2bfa2fb
vijayvenkatj <vijayvenkatj@gmail.com>
2025-05-17 12:06:39 +0530

    Changes to Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StickyGlassNavbar from "@/components/navbar";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin-ext"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <StickyGlassNavbar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
